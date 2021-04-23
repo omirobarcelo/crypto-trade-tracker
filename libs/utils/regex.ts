4dae7759ffa329514d412e331d4a05067ba5b488
Oriol <magna.OMIRO.shogun@gmail.com>
2021-04-23 11:59:41 +0200

    Added regex to match price with amount and fiat currency

--- libs/utils/regex.ts
+++ libs/utils/regex.ts
@@ -1,1 +1,5 @@
 export const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+
+export const FIAT_PRICE_AMOUNT = 1;
+export const FIAT_PRICE_CURRENCY = 3;
+export const fiatPriceRegex = /^(-?\d+(\.\d+)?)\s*([a-zA-Z]{3})$/;

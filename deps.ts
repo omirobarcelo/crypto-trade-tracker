634743420352b47370675e8b5d027339dc03d77a
Oriol <magna.OMIRO.shogun@gmail.com>
2021-04-23 11:54:16 +0200

    Added writeJson, chalkin, and InputLoop to deps

--- deps.ts
+++ deps.ts
@@ -1,1 +1,7 @@
-export { readJson } from 'https://deno.land/x/jsonfile@1.0.0/mod.ts';
+export { writeJson } from 'https://deno.land/x/jsonfile@1.0.0/mod.ts';
+
+import { chalkin as Chalkin } from 'https://deno.land/x/chalkin@v0.1.3/mod.ts';
+export const chalkin = new Chalkin();
+
+import InputLoop from 'https://deno.land/x/input@2.0.2/index.ts';
+export const Input = InputLoop;
